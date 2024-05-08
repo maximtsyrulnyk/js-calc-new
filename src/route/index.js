467511e260fbe6bee5f1b6020c28f321c56ca4cf
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-05-08 14:05:12 +0300

    have some troubles in Practice JS Calculator

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
+const test2 = require('./test2')
+const calc = require('./calc')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/test', test2)
+router.use('/calc', calc)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
